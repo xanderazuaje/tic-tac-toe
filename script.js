d4e2285d9a110fc4e9e2336e4bd62676218b2fe6
Charonmortis <101378229+Charonmortis@users.noreply.github.com>
2023-01-28 20:15:42 +0100

    created a game core function

---
+++ script.js
@@ -0,0 +1,42 @@
+const make2DArrayOf = (number1,number2)=>{ return new Array(number1).fill(null).map(()=>new Array(number2).fill(null)) }
+
+let gameStatus = make2DArrayOf(3,3)
+
+const game = () => {
+    const setPosition = (square) => {
+        position = {
+            x: square.dataset.x,
+            y: square.dataset.y
+        };
+        gameStatus[position.y][position.x] = gameStatus[position.y][position.x]  === null ? 'X' : 0;
+    } ;
+    
+    const aiPlay = () => {
+        const getRandomIndex = (number) => { return Math.floor(Math.random() * number ) }
+        position = {
+            x: getRandomIndex(3),
+            y: getRandomIndex(3)
+        };
+        gameStatus[position.y][position.x] =  gameStatus[position.y][position.x]  === null ? 'O' : aiPlay();
+    }
+    
+    const play = (square) => {
+        setPosition(square)
+        aiPlay()
+        console.log(gameStatus)
+    }
+
+    const clearPosition = () => gameStatus = make2DArrayOf(3,3);
+
+    return { clearPosition, play }
+}
+let currentGame = game()
+
+let play = {
+    dataset:{
+        x: 0,
+        y: 0
+    }
+}
+
+currentGame.play(play)
